386e6b45c80361d42ed8e4de2d9e357872f19c9f
William AD <w.albertusd@gmail.com>
2018-10-27 17:05:15 +0700

    refactor how rectangle draw

--- test1/Draw/Object/Rectangle.cs
+++ test1/Draw/Object/Rectangle.cs
@@ -1,4 +1,7 @@
 ﻿using Gdk;
+using System.Collections.Generic;
+using test1.Common;
+
 namespace test1.Draw.Object
 {
 	public class Rectangle : IObject
@@ -6,14 +9,20 @@ namespace test1.Draw.Object
 		Common.PointD startPoint;
 		Common.PointD endPoint;
 		Cairo.Color color;
+		List<Common.PointD> points;
 
 		public Rectangle(Common.PointD startPoint, Common.PointD endPoint)
 		{
+			points = new List<Common.PointD>();
 			color = new Cairo.Color(0, 0, 0);
 			this.startPoint = startPoint;
 			this.endPoint = endPoint;
+			points.Add(new Common.PointD(startPoint.X, startPoint.Y));
+			points.Add(new Common.PointD(endPoint.X, startPoint.Y));
+			points.Add(new Common.PointD(endPoint.X, endPoint.Y));
+			points.Add(new Common.PointD(startPoint.X, endPoint.Y));
 		}
-
+       
 		public void ChangeColor(int r, int g, int b)
         {
             color = new Cairo.Color(r, g, b);
@@ -28,10 +37,10 @@ namespace test1.Draw.Object
 		{
 			Cairo.Context g = CairoHelper.Create(window);
 			g.LineWidth = 5;
-			g.MoveTo(startPoint.X, startPoint.Y);
-			g.LineTo(endPoint.X, startPoint.Y);
-			g.LineTo(endPoint.X, endPoint.Y);
-			g.LineTo(startPoint.X, endPoint.Y);
+			g.MoveTo(points[0].X, points[0].Y);
+			foreach(Common.PointD point in points) {
+				g.LineTo(point.X, point.Y);
+			}
 			g.SetSourceColor(color);
 			g.ClosePath();
 			g.Stroke();
@@ -45,10 +54,14 @@ namespace test1.Draw.Object
 
 		public void Translate(double x, double y)
         {
+			foreach (Common.PointD point in points) {
+				point.X += x;
+				point.Y += y;
+			}
             startPoint.X += x;
             endPoint.X += x;
             startPoint.Y += y;
-            endPoint.Y += y;
+			endPoint.Y += y;
         }
 	}
 }
