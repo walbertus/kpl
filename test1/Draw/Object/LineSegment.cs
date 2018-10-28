2e5c99e5c21923361fdc1cc9bffcd03c743ad7a1
William AD <w.albertusd@gmail.com>
2018-10-28 09:30:14 +0700

    refactor line segment

--- test1/Draw/Object/LineSegment.cs
+++ test1/Draw/Object/LineSegment.cs
@@ -1,4 +1,7 @@
 ﻿using Gdk;
+using System.Collections.Generic;
+using test1.Common;
+
 namespace test1.Draw.Object
 {
 	public class LineSegment: IObject
@@ -6,12 +9,20 @@ namespace test1.Draw.Object
 		Common.PointD startPoint;
 		Common.PointD endPoint;
 		Cairo.Color color;
+		List<PointD> points;
 		
 		public LineSegment(Common.PointD startPoint, Common.PointD endPoint)
         {
+			points = new List<PointD>();
+			points.Add(new PointD(startPoint.X, startPoint.Y));
+			points.Add(new PointD(endPoint.X, endPoint.Y));
 			color = new Cairo.Color(0, 0, 0);
-			this.startPoint = startPoint;
-			this.endPoint = endPoint;
+			double minX = System.Math.Min(startPoint.X, endPoint.X);
+            double maxX = System.Math.Max(startPoint.X, endPoint.X);
+            double minY = System.Math.Min(startPoint.Y, endPoint.Y);
+            double maxY = System.Math.Max(startPoint.Y, endPoint.Y);
+            this.startPoint = new PointD(minX, minY);
+            this.endPoint = new PointD(maxX, maxY);
         }
 
 		public void ChangeColor(int r, int g, int b)
@@ -28,9 +39,13 @@ namespace test1.Draw.Object
 		{
 			Cairo.Context g = CairoHelper.Create(window);
             g.LineWidth = 5;
-			g.MoveTo(startPoint.X, startPoint.Y);
-			g.LineTo(endPoint.X, endPoint.Y);
-			g.SetSourceColor(color);
+            g.MoveTo(points[0].X, points[0].Y);
+            foreach (PointD point in points)
+            {
+                g.LineTo(point.X, point.Y);
+            }
+            g.SetSourceColor(color);
+            g.ClosePath();
             g.Stroke();
 		}
 
@@ -42,9 +57,14 @@ namespace test1.Draw.Object
 
 		public void Translate(double x, double y)
 		{
-			startPoint.X += x;
-			endPoint.X += x;
-			startPoint.Y += y;
+			foreach (PointD point in points)
+            {
+                point.X += x;
+                point.Y += y;
+            }
+            startPoint.X += x;
+            endPoint.X += x;
+            startPoint.Y += y;
             endPoint.Y += y;
 		}
 	}
